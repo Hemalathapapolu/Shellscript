49e4f0bcc7b5a4dacfbacae61b51f78b48f97458
Hema <hemapapolu9@gmail.com>
2024-04-24 16:12:56 +1000

    first commit

--- pacakage.loop.sh
+++ pacakage.loop.sh
@@ -25,7 +25,7 @@ for i in $@
 do
     echo " pacakage to install $i "
     dnf list installed $i &>>LOGFILE
-    if [ $? -eq 0]
+    if [$? -eq 0]
     then 
         echo " $i is already installed .... SKIPING "
     else
