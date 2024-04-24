d49d430b6e8bc05db1fdb2e02392e26c32623b18
Hema <hemapapolu9@gmail.com>
2024-04-24 15:42:03 +1000

    first commit

--- expense.sh
+++ expense.sh
@@ -4,13 +4,14 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME.log
 
 VALIDATE(){
-    if ($1 -ne 0 )
+    if ( $1 -ne 0 )
     then 
         echo "$2 is Failure"
     else
         echo "$2 is Success"
     fi
 }
+
 if ($USERID -ne 0)
 then
     echo "Try with root access"
