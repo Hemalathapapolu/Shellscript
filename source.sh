708fb4eaf8e70b493d66de5a981085ee30d29c9a
Hema <hemapapolu9@gmail.com>
2024-04-30 20:49:38 +1000

    first commit

---
+++ source.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+COURSE=($DevOps)
+
+echo " Before source learing $COURSE "
+
+source ./source1.sh
+
+echo " Source1 course $COURSE "
---
+++ source1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+COURSE=$(Terraform)
+
+echo " Print after source $COURSE "
