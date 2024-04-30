9bfc30d80fdcd200becc61603a15ac019ce808ff
Hema <hemapapolu9@gmail.com>
2024-04-30 21:25:22 +1000

    first commit

--- SOURCE/expense_source.sh
+++ SOURCE/expense_source.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
-
+error(){
+    echo " Error at $1: $2 "
+    trap ' "failure at:$Lineno" "failure at:$bash_command" ' ERR
+    }
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S) 
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
